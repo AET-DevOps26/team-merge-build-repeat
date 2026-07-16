342a7ad5e3d16702484e504e47696de512a73a35
Nico Matuschyk <nico.matuschyk@tum.de>
2026-07-16 20:12:39 +0200

    Update Docker configurations for PostgreSQL services and add custom entrypoint script

--- scripts/init-pg-stat-statements.sql
+++
@@ -1,3 +0,0 @@
--- Initialize pg_stat_statements extension for fresh databases
--- Existing databases will have the extension created by the startup migration script
-CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
