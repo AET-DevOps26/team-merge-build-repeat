358e27f63635ea49753c6afa7e5833bf2e2ce68c
ge87geb <marvin.hanke@tum.de>
2026-07-17 09:34:24 +0200

    CR

--- services/application/src/main/java/com/sudokuai/merge_build_repeat/config/RestTemplateConfig.java
+++ services/application/src/main/java/com/sudokuai/merge_build_repeat/config/RestTemplateConfig.java
@@ -1,7 +1,10 @@
 package com.sudokuai.merge_build_repeat.config;
 
+import java.time.Duration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +12,9 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(5));
+        requestFactory.setReadTimeout(Duration.ofSeconds(5));
+        return new RestTemplate(requestFactory);
     }
 }
